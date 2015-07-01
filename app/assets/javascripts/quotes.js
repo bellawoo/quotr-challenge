c9c3ecc19584a712d33c8e21ccec05f66273c797
Bella <woo.bella@gmail.com>
2015-07-01 11:00:31 -0400

    solutions

---
+++ app/assets/javascripts/quotes.js
@@ -0,0 +1,17 @@
+$(document).on("ready", function() {
+  $("that_button").on("click", update_saying)
+})
+
+var update_saying = function() {
+  $.ajax("/random", {
+    error: function() { alert("Something went wrong") },
+    success: function(data) {
+      $("#quote_box").text(data.text)
+      $("#author_box").text("- " + data.author)
+    }
+  })
+}
+
+
+
+
